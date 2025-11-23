27f24b7519a7c1bdfb2d8572b5d4d78ac1ffb5ed
GaetanoGr02 <gaetano1.grasso@mail.polimi.it>
2025-11-23 17:10:03 +0100

    .

--- Codici/Trajectory/LauncherTrajectory/propEvent.m
+++ Codici/Trajectory/LauncherTrajectory/propEvent.m
@@ -7,7 +7,7 @@ h = norm(x(1:3)) - mission.environment.rEarth + 0.2;
 value(1) = x(end) - (m0 - mP);
 
 isterminal(1) = 1;       
-direction(1)  = 0;     
+direction(1)  = -1;     
 
 value(2) = h;
 
