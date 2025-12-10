a40602b65e4197d94af9d7b9810408879b7e3210
MatiFormi <matildefor02@gmail.com>
2025-12-10 11:23:27 +0100

    Add and correct some functions

---
+++ Codici/Control/InertaEvaluation.m
@@ -0,0 +1,35 @@
+function [inertia] = InertaEvaluation(mission, configuration, launcher, totalStageNumber )
+% INERTIAEVALUATION evaluate the inertia matrix of launcher + capsule
+% Capsule data (assumption of conical shape) -- > from Sforza Book
+mCapsule = mission.capsule.weight ;
+rCapsule = mission.capsule.radius ;
+hCapsule = mission.capsule.height ;
+
+inertia.capsule = 3 * mCapsule * hCapsule^2 / 80 * ( 1 + 4 * (rCapsule / hCapsule)^2) ;
+
+% Stage number
+stageNumber = launcher(1) ;
+inertia.stage = 0 ;
+inertia.interStage = 0 ;
+
+% launcher Data
+for ii = totalStageNumber : -1 : stageNumber
+   hStageIter = configuration.geometry.stage{ii}.lenght;
+   rStageIter = configuration.geometry.stage{ii}.radius;
+   mStageIter = configuration.stage{ii}.mStage ;
+
+   hInterstageIter = configuration.geometry.stage{ii}.interstage.lenght ;
+   rInterstageIter = configuration.geometry.stage{ii-1}.radius;
+   mInterstageIter = mer.stage{ii}.interStage ;
+   inertia.stage(ii) = mStageIter * hStageIter^2 / 12 * (1 + 3 * (rStageIter / hStageIter)^2 ) ;
+   inertia.stage = inertia.stage + inertia.stage(ii) ;
+
+   inertia.interstage(ii) = 3 * mInterstageIter * (rStageIter^5 - rInterstageIter^5) / 10 / (rStageIter^3 - rInterstageIter^3)...
+       - mInterstageIter * hInterstageIter^2 / 16 * ( (rStageIter^2 + 2 * ...
+        rStageIter * rInterstageIter + 3 * rInterstageIter^2) / (rStageIter^2 + 2 * rStageIter * rInterstageIter + rInterstageIter^2)) ;
+   inertia.interStage = inertia.interStage + inertia.interStage(ii) ;
+end
+
+inertia = inertia.capsule + inertia.stage + inertia.interStage ;
+
+end
--- Codici/Control/mainControl.m
+++ Codici/Control/mainControl.m
@@ -4,6 +4,9 @@ clear all
 
 % Optimal Solution form an old GA
 launcher = [2,1,4,4,0.4056,0.4016,0.7];
+
+[mission,settings] = dataStructGlobal;
+
 for i = 1:launcher(1)
     configuration.stage{i}.engine = mission.engines{launcher(1+i)};
 end
@@ -15,35 +18,69 @@ end
                         [],[],[],[],settings.lowerBoundsGA,settings.upperBoundsGA, ...
                         @(x) nlconGATraj( reshape(x,settings.nOptPointsTraj,2,launcher(1)),launcher,configuration, mission,settings),settings.gaTrajOptions);
 
-%% Plots
+
 thrustData = reshape(xGATraj,settings.nOptPointsTraj,2,2);
 
 % Upload state and time from Traj2D
 load('stateCollocation.mat') ;
 load('timeCollocation.mat') ;
 
+% Manca il reshape di guidancePoints
+
+% Manca Rotazione sul piano 2 degli stateCollocation
+
 % Initial Condition
-x0 = stateCollocation(:,1,1) ;
+y0 = stateCollocation(:,1,1) ;
 
 % Upload mission data
-[mission,opt] = dataStruct;
 tSpan = [0 500];
 
-Nstage = 3 ;
+Nstage = launcher(1) ;
 
-idx = ceil(linspace(1, 100 , 25)) ;
-% Point for the contorl law of the Guidance
-guidancePoints = zeros(7,length(idx), Nstage) ; 
-guidanceTime = zeros(length(idx), Nstage) ;
+%% GA for tuning gains
+
+% lowerBounds = [10 1 10 10 10 10 100 100]'; 
+% upperBounds = [1000 1 1000 1000 1000 1000 1000 1000]'; 
+lowerBounds = 1 * ones(8,1) ; 
+upperBounds = 1000 * ones(8,1) ;
+nVars = length(lowerBounds) ;
+
+gains0 = [50 0 10 1 0 60 100 1000]' ;
+intCon = 1:8 ;
+
+options_ga = optimoptions("ga", ...
+    "Display","iter", ...
+    "MaxGenerations",30, ...
+    "PopulationSize",200,...
+    "UseParallel",true,...
+    "FunctionTolerance", 1e-4,...
+    "MaxStallGenerations", 10,...
+    'EliteCount',  6);
 
 
-for ii = 1 : Nstage
- guidancePoints(:,:,ii) = stateCollocation(:, idx, ii) ;
- guidanceTime(:,ii)   = timeCollocation(idx, ii) ;
+[gains] = ga(@(x) findGains(x,guidancePoints,y0,tSpan), nVars, [],[],[],[],lowerBounds,upperBounds, [] ,intCon, options_ga) ;
+
+
+options = odeset('RelTol',1e-6,'AbsTol',1e-6);
+[t, sol] = ode113(@(t,x) launcherDynamicsAndControlECI(t, x,thrustData, mission, configuration, launcher, stageNumber, guidancePoints, gains), tSpan, y0,options);
+
+
+
+
+function [output] = findGains(x,guidancePoints,y0,tSpan)
+
+gains = x ;
+
+if ~iscolumn(gains)
+    gains = gains' ;
 end
 
-% prova di gains
-gains = [1, 1, 1, 1] ;
+
+options = odeset('RelTol',1e-6,'AbsTol',1e-6);
+[t, sol] = ode113(@(t,x) launcherDynamicsAndControlECI(t, x,thrustData, mission, configuration, launcher, stageNumber, guidancePoints, gains), tSpan, y0,options);
 
 
-%%
+output = sum(sol(:, 1:2)' - guidancePoints(1:2,:)) + sum(sol(:, 3:4)' - guidancePoints(3:4, :)) ;
+
+
+end
