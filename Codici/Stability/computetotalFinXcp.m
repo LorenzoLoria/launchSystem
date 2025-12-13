2a7bfd36d5739a8a26e1b3c520e4e159896f195f
ConstanceBalansard <constance.balansard@student-cs.fr>
2025-12-13 14:03:16 +0100

    update fins codes

--- Codici/Stability/computetotalFinXcp.m
+++ Codici/Stability/computetotalFinXcp.m
@@ -1,54 +1,73 @@
-function Xcp_total = computetotalFinXcp(opt, mission, vlauncher, vsound, be, Se, Sref)
-% computeTotalFinXcp Computes the total center of pressure of the vehicle including fins
+function Xcp_total = computetotalFinXcp(opt, mission)
+% Computes the total center of pressure of the vehicle including fins for both stages
 %
 % Inputs:
-%   N         : current stage number (2=stage1, 1=stage2, 0=no stage)
-%   alpha     : angle of attack [rad]
-%   lc1, lc2  : stage 1 and 2 lengths [m]
-%   lco       : cone length [m]
-%   vlauncher : launcher speed [m/s]
-%   vsound    : speed of sound [m/s]
-%   be        : fin axial base [m]
-%   Se        : fin surface area [m^2]
-%   Sref      : vehicle reference area [m^2]
+%   opt       : options structure (contains nStages)
+%   mission   : mission structure (contains geometry, aerodynamics, etc.)
 %
 % Output:
 %   Xcp_total : combined center of pressure [m]
 
-    N     = opt.nStages;
-    cmac = mission.aerodynamics.finsGeom.cmac ;
+    N = opt.nStages; % Current stage number (2=both stages, 1=stage 2 only, 0=no stages)
 
     % --- Body CP
     Xcp_body = computeXcpcomputeXcp(mission, opt);
+    % --- Weighted average to get total Xcp
+    S_body = mission.aerodynamics.bodyGeom.Aref;
 
-    % --- If stage 1, include fins
+    % --- Data fins 1st stage 
+
+    delta_le = mission.aerodynamics.finsGeomStage.delta_le;
+    lambda_le = mission.aerodynamics.finsGeomStage.lambda_le;
+    bfin1   = mission.aerodynamics.finsGeomStage.bfin;
+    be1 = 2*bfin1;
+    Sfin1   = mission.aerodynamics.finsGeomStage.Sfin;
+    Se1 = 2*Sfin1;
+    cmac1 = mission.aerodynamics.finsGeomStage1.cmac;
+    tmac1 = mission.aerodynamics.finsGeomStage1.tmac;
+
+    % --- Center of pressure fins 1st stage
+
+
+    Xcp_fin1 = computeFinXcp(mission, be1, cmac1, Se1, maxQData);
+        [CN_fin1, ~, ~] = AerodynCoefFins_new(mission, cmac1, lambda_le, Se1, delta_le, tmac1);
+
+    % --- Data fins 2nd stage 
+
+    bfin2   = d2/d1*bfin1;
+    be2 = 2*bfin2;
+    Sfin2   = (d2/d1)^2*Sfin1;
+    Se2 = 2*Sfin2;
+    cmac2 = d2/d1*cmac1; 
+    tmac2 = d2/d1*tmac1;
+
+    % --- Center of pressure fins 2nd stage
+
+    Xcp_fin2 = computeFinXcp(mission, be2, cmac2, Se2, maxQData);
+        [CN_fin2, ~, ~] = AerodynCoefFins_new(mission, cmac2, lambda_le, Se2, delta_le, tmac2);
+
+
+    
+    % --- Fin CP and normal force coefficient
+    
+    % --- 2 stages 
     if N == 2
-        % Fin CP
-        Xcp_fin = computeFinXcp(vlauncher, vsound, cmac, be, Se);
+        
+       Xcp_total = (Xcp_body*S_body + Xcp_fin1*Se1*CN_fin1 + Xcp_fin2*Se2*CN_fin2) / ...
+                    (S_body + Se1*CN_fin1 + Se2*CN_fin2);
 
-        % Estimate fin normal force coefficient
-        alpha_p = mission.alpha;    % small angle, in radians
-        b       = be/2;             % fin span
-      % b = mission.aerodynamics.finsGeom.b ;
-        delta_le = 20;              % example leading edge sweep [deg]
-      % delta_le = mission.aerodynamics.finsGeom.delta_le ;
-        lambda_le = 36.9;           % example base angle [deg]
-      % lambda_le = mission.aerodynamics.finsGeom.lambda_le
-      
-        tmac = 0.1*cmac;            % max thickness
-      % tmac = mission.aerodynamics.finsGeom.tmac ;
-      
-        q = 0.5*1.225*vlauncher^2;  % dynamic pressure, rho=1.225 kg/m3
+        
+    end
 
-        [CN_fin, ~, ~] = AerodynCoefFins_new(alpha_p, be^2/Se, vlauncher, vsound, Se, q, Sref, cmac, delta_le, lambda_le, b, tmac);
+    % --- 1 stages
+    if N == 1
 
-        % Body area
-        S_body = Sref - Se;
+        Xcp_total = (Xcp_body*S_body + Xcp_fin1*Se1*CN_fin1) / ...
+                    (S_body + Se1*CN_fin1);
 
-        % Weighted average to get total Xcp
-        Xcp_total = (Xcp_body*S_body + Xcp_fin*Se*CN_fin) / (S_body + Se*CN_fin);
+    % --- 0 stages
     else
-        % No fins: total CP = body CP
+        
         Xcp_total = Xcp_body;
     end
 end
